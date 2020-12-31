a7639ed580fb90c11b35b37491aa3d629eeeba72
Xiaomi <andriiyarotskiy@gmail.com>
2020-12-31 13:46:44 +0200

    5.9 Шифрование пароля

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const {Router} = require('express')
 const router = Router()
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 
 router.get('/login', async (req, res) => {
@@ -23,7 +24,8 @@ router.post('/login', async (req, res) => {
         const {email, password} = req.body
         const candidate = await User.findOne({email})
         if (candidate) {
-            const areSame = password === candidate.password
+            // compare асинхронный метод сравнивает пароли
+            const areSame = await bcrypt.compare(password, candidate.password)
             if (areSame){
                 req.session.user = candidate
                 req.session.isAuthenticated = true
@@ -55,8 +57,11 @@ router.post('/register', async (req, res) => {
         if (candidate) {
             res.redirect('/auth/login#register')
         } else {
+            // метод hash возвращает промис, асинхронный метод
+            const hashPassword =await bcrypt.hash(password, 10)
+
             const user = new User({
-                email, name, password, cart: {items: []}
+                email, name, password: hashPassword, cart: {items: []}
             })
             await user.save()
             res.redirect('/auth/login#login')
