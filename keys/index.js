4f816933fd80fedc8c2f4af1508d72a392d20387
Xiaomi <andriiyarotskiy@gmail.com>
2021-01-13 15:54:07 +0200

    6.2 Настройка почтового сервиса (email в node.js отправляется через доп пакеты)

--- keys/index.js
+++ keys/index.js
@@ -1,4 +1,5 @@
 module.exports = {
-    MONGODB_URI: "mongodb+srv://Andrii:N9RofYUiK19lRPRp@cluster0.cmkhw.mongodb.net/shop",
-    SESSION_SECRET: 'some secret value'
+    MONGODB_URI: 'mongodb+srv://Andrii:N9RofYUiK19lRPRp@cluster0.cmkhw.mongodb.net/shop',
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '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'
 }
