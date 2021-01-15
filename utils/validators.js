8e88d925d4fa1d0a4e35af605df1cb54327eded7
Xiaomi <andriiyarotskiy@gmail.com>
2021-01-15 12:55:48 +0200

    7.4 Улучшение данных

--- utils/validators.js
+++ utils/validators.js
@@ -2,22 +2,30 @@ const {body} = require('express-validator')
 const User = require('../models/user')
 
 exports.registerValidators = [
-    body('email').isEmail().withMessage('Please enter correct email').custom(async (value, {req}) => {
-        try {
-            const user = await User.findOne({email: value})
-            if (user) {
-                return Promise.reject('This mail is already taken')
+    body('email')
+        .isEmail().withMessage('Please enter correct email')
+        .custom(async (value, {req}) => {
+            try {
+                const user = await User.findOne({email: value})
+                if (user) {
+                    return Promise.reject('This mail is already taken')
+                }
+            } catch (e) {
+                console.log(e)
             }
-        } catch (e) {
-            console.log(e)
-        }
-    }),
-    body('password', 'Password must have at least 6 characters').isLength({min: 6, max: 56}).isAlphanumeric(),
+        }).normalizeEmail(),
+    body('password', 'Password must have at least 6 characters')
+        .isLength({min: 6, max: 56})
+        .isAlphanumeric()
+        .trim(),
     body('confirm').custom((value, {req}) => {
         if (value !== req.body.password) {
             throw new Error('Пароли должны совпадать')
         }
         return true
-    }),
-    body('name').isLength({min: 3}).withMessage('Name must be 3 or more characters')
+    }).trim(),
+    body('name')
+        .isLength({min: 3})
+        .withMessage('Name must be 3 or more characters')
+        .trim()
 ]
